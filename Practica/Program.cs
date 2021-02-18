1b30f086cce75832dddfd403f8f3154f370a28e3
JhonatanMolina <JhonatanMolina@JHONATAN>
2021-02-18 15:46:11 +0100

    Agregar archivos de proyecto.

---
+++ Practica/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero1 = 0;
+            int numero2 = 0;
+            int multiplicar = 0;
+
+            Console.Write("Digite un numero");
+            numero1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Digite un numero");
+            numero2 = Convert.ToInt32(Console.ReadLine());
+            multiplicar = numero1 * numero2;
+            Console.WriteLine($"La multiplicacion es {multiplicar}");
+
+            Console.ReadKey();
+        }
+    }
+}
